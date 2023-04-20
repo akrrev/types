96925b8bf3438dca957657c99b67062ceb7fba71
Alexander Kristiansen <akr@revealit.dk>
2023-04-20 16:23:20 +0200

    Add type helper for making optional properties nullable recursively

--- src/nullable.ts
+++ src/nullable.ts
@@ -11,3 +11,10 @@ import { GetOptionalKeys } from "./optional";
 type MakeOptionalNullable<T> = {
   [P in keyof T]: P extends GetOptionalKeys<T> ? T[P] | null : T[P];
 }
+
+/**
+ * Makes all optional keys in a given type nullable recursively.
+ */
+type MakeOptionalNullableRecursive<T> = T extends Object ? {
+  [P in keyof T]: P extends GetOptionalKeys<T> ? MakeOptionalNullableRecursive<T[P]> | null : MakeOptionalNullableRecursive<T[P]>
+} : T;
