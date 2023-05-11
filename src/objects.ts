495c3f9996c4c466d411ebea90c77384ee9703b0
Alexander Kristiansen <akr@revealit.dk>
2023-05-11 13:38:11 +0200

    Restructure types related to objects

--- src/objects.ts
+++ src/objects.ts
@@ -1,9 +1,15 @@
-import { GetOptionalKeys } from "./optional";
-
 /**
  * @file
- * Type helpers related to null values.
+ * Type helpers related to objects.
  */
+
+/**
+ * Gets all optional keys in a given type.
+ */
+export type GetOptionalKeys<T> = {
+  [K in keyof T]: {} extends Pick<T, K> ? K : never
+}[keyof T];
+
 
 /**
  * Makes all optional keys in a given type nullable.
@@ -18,3 +24,9 @@ type MakeOptionalNullable<T> = {
 type MakeOptionalNullableRecursive<T> = T extends Object ? {
   [P in keyof T]: P extends GetOptionalKeys<T> ? MakeOptionalNullableRecursive<T[P]> | null : MakeOptionalNullableRecursive<T[P]>
 } : T;
+
+
+/**
+ * Marks the given keys as required properties in the object.
+ */
+export type RequireKeys<T, K extends keyof T> = T & { [P in K]-?: T[P] };
